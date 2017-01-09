0c05ee56b9f6450372dcaf0b2d7ceeb842223626
Chris Vig <chris@invictus.so>
2017-01-08 20:26:23 -0600

    Add tracer object.

---
+++ src/tracer.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file	tracer.cpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2017/01/08
+ */
+
+/* -- Includes -- */
+
+#include "tracer.hpp"
+
+/* -- Namespaces -- */
+
+using namespace learncpp;
+
+/* -- Variables -- */
+
+int tracer::s_next_id = 0;
+
+/* -- Procedures -- */
+
+void tracer::trace(const std::string& method, const std::string& file, int line)
+{
+  std::cout << "[tracer id=" << m_id << " name=\"" << m_name << "\"] " << method << std::endl;
+}
---
+++ src/tracer.hpp
@@ -0,0 +1,86 @@
+/**
+ * @file	tracer.hpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2017/01/08
+ */
+
+#pragma once
+
+/* -- Includes -- */
+
+#include <iostream>
+#include <string>
+
+/* -- Types -- */
+
+namespace learncpp
+{
+
+  /**
+   * Class for tracing object lifecycle.
+   */
+  class tracer
+  {
+  public:
+
+#define TRACE() this->trace(__PRETTY_FUNCTION__, __FILE__, __LINE__)
+
+    /** Standard class constructor. */
+    tracer(const std::string& name)
+      : m_id(s_next_id++),
+        m_name(name)
+    {
+      TRACE();
+    }
+
+    /** Copy constructor. */
+    tracer(const tracer& other)
+      : m_id(s_next_id++),
+        m_name(other.m_name)
+    {
+      TRACE();
+    }
+
+    /** Assignment operator. */
+    tracer& operator =(const tracer& other)
+    {
+      m_name = other.m_name;
+      TRACE();
+      return *this;
+    }
+
+    /** Move constructor. */
+    tracer(tracer&& other)
+      : m_id(s_next_id++),
+        m_name(std::move(other.m_name))
+    {
+      TRACE();
+    }
+
+    /** Move assignment operator. */
+    tracer& operator =(tracer&& other)
+    {
+      m_name = std::move(other.m_name);
+      TRACE();
+      return *this;
+    }
+
+    /** Destructor. */
+    ~tracer()
+    {
+      TRACE();
+    }
+
+#undef TRACE
+
+  private:
+
+    static int s_next_id;
+    const int m_id;
+    std::string m_name;
+
+    void trace(const std::string& method, const std::string& file, int line);
+
+  };
+
+}
